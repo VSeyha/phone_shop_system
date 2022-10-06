f18b6db8be07b1604dc40425f56f367ed7578d0d
Vet Seyha <89523604+VSeyha@users.noreply.github.com>
2022-10-06 21:44:29 +0700

    initial commit

---
+++ src/main/java/se/phoneshop/ServletInitializer.java
@@ -0,0 +1,13 @@
+package se.phoneshop;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PhoneshopApplication.class);
+	}
+
+}
---
+++ src/test/java/se/phoneshop/PhoneshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.phoneshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
